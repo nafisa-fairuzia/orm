46f3c14bfb6790aa9dd42cd29d301bcfb6799848
nafisa-fairuzia <nafisafairuzia@gmail.com>
2025-02-01 07:41:03 +0700

    Add files via upload

---
+++ many to many/web.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\MahasiswaController;
+
+Route::get('/mahasiswa',[MahasiswaController::class, 'index']);
+
+
+
+
+// Route::get('/produk',[ProdukController::class,'index']);
+
+// Route::get('/penulis',[PenulisController::class,'index']);
+
+// Route::get('/gallery', function() {
+//     return view('gallery'); 
+// });
+
+// Route::get('/sekolah/{sekolah}/{guru}/{alamat}', function ($sekolah, $guru, $alamat) {
+//     return "Nama Sekolah: $sekolah, Nama Guru: $guru, Alamat: $alamat";
+// });
+
+
+// Route::get('/', [DokterController::class, 'index']);
+// Route::get('/dokter', [DokterController::class, 'tampil']);
